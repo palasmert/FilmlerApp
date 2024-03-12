1806a79391975a4b661d0ef25599da1d8419caac
palasmert <134235672+palasmert@users.noreply.github.com>
2024-03-12 23:46:56 +0300

    finished

--- FilmlerApp/FilmlerApp/UI/TableView/FilmlerHucre.swift
+++ FilmlerApp/FilmlerApp/UI/TableView/FilmlerHucre.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 
+protocol HucreProtocol {
+    func sepeteEkleTikle(indexPath: IndexPath)
+}
+
 class FilmlerHucre: UICollectionViewCell {
     @IBOutlet weak var imageViewFilm: UIImageView!
     @IBOutlet weak var labelFiyat: UILabel!
     
+    var hucreProtocol: HucreProtocol?
+    var indexPath: IndexPath?
     
     @IBAction func buttonSepeteEkle(_ sender: Any) {
+        hucreProtocol?.sepeteEkleTikle(indexPath: indexPath!)
     }
     
 }
--- FilmlerApp/FilmlerApp/UI/Views/DetaySayfa.swift
+++ FilmlerApp/FilmlerApp/UI/Views/DetaySayfa.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class DetaySayfa: UIViewController {
 
+    @IBOutlet weak var labelFilm: UILabel!
+    @IBOutlet weak var imageViewFilm: UIImageView!
+    @IBOutlet weak var labelFiyat: UILabel!
+    
+    var film: Filmler?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let f = film {
+            labelFilm.text = f.ad
+            imageViewFilm.image = UIImage(named: f.resim!)
+            labelFiyat.text = "\(f.fiyat!) ₺"
+        }
+        
     }
     
 
